1c2c6ef1396837665465211e817f37ed4fbad6da
Drew Leonard <andy.j.leonard@gmail.com>
2018-09-05 11:28:16 +0100

    Drew being a bastard

--- conversion.py
+++ conversion.py
@@ -1,10 +1,10 @@
 def kelvin_to_celsius(temp):
     return temp - 273.15
 
-def celsius_to_fhar(temp):
+def celsius_to_fahr(temp):
     return temp * (9/5) + 32
 
-def kelvin_to_fhar(temp):
+def kelvin_to_fahr(temp):
     temp_c = kelvin_to_celsius(temp)
-    result = celsius_to_fhar(temp_c)
+    result = celsius_to_fahr(temp_c)
     return result
